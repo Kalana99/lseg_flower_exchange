9a4041511cdc2975be6a1dd11b6f366adfd62f7f
Kalana Rubasinghe <kalanarajika99@gmail.com>
2023-12-15 18:33:00 +0530

    prop

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -2,21 +2,30 @@
 
 #include <iostream>
 
+// quty being equal is a separate case
+// always assign the price of the one in the order book
+
 OrderBook::OrderBook() {}
 
 void OrderBook::addOrder(Order entry, int priority_num)
 {
-
+    // make global
     std::string orderId = "ord" + std::to_string(orderCounter++);
     std::vector<std::string> orderRow = {orderId, std::to_string(entry.quantity), std::to_string(entry.price), std::to_string(priority_num)};
 
     if (entry.side == 2) // sell order
     {
-        if (entry.price <= std::stoi(this->buyOrder[0][2]))
+        if (this->buyOrder.size() > 0 && entry.price <= std::stoi(this->buyOrder[0][2]))
         {
-            if (entry.quantity <= std::stoi(this->buyOrder[0][1]))
+            if (entry.quantity < std::stoi(this->buyOrder[0][1]))
             {
+                // no orderbook record - add entry to report
                 return; // there' already a buyer for the selling product
+            }
+            else if(entry.quantity == std::stoi(this->buyOrder[0][1]))
+            {
+                // remove orderbook record and add both entries to report
+                return;
             }
             else
             {
@@ -28,7 +37,7 @@ void OrderBook::addOrder(Order entry, int priority_num)
     }
     else if (entry.side == 1) // buy order
     {
-        if (entry.price >= std::stoi(this->sellOrder[0][2]))
+        if (this->sellOrder.size() > 0 && entry.price >= std::stoi(this->sellOrder[0][2]))
         {
             if (entry.quantity <= std::stoi(this->sellOrder[0][1]))
             {
