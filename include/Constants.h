524e0b57654426e5afc8c4a364d45297a651a203
Kalana Rubasinghe <kalanarajika99@gmail.com>
2023-12-15 09:04:21 +0530

    constants

---
+++ include/Constants.h
@@ -0,0 +1,13 @@
+// Constants.h
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+#include <set>
+
+namespace MyConstants {
+
+    const std::set<std::string> INSTRUMENTS = {"Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
+}
+
+#endif
