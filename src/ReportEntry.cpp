7730c42734971d6b5e289cea5e364b3d9d4f52ed
Kalana Rubasinghe <kalanarajika99@gmail.com>
2023-12-15 12:50:06 +0530

    report entry constructor

--- src/ReportEntry.cpp
+++ src/ReportEntry.cpp
@@ -1,22 +1,50 @@
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
 #include "ReportEntry.h"
+#include "Globals.h"
 
 ReportEntry::ReportEntry(std::vector<std::string> row) {
     
     client_order_id = row[0];
-    order_id = row[1];
-    instrument = row[2];
-    side = std::stoi(row[3]);
-    quantity = std::stoi(row[4]);
-    price = std::stod(row[5]);
-    reason = row[6];
+    instrument = row[1];
+    side = std::stoi(row[2]);
+    quantity = std::stoi(row[3]);
+    price = std::stod(row[4]);
+    status = std::stoi(row[5]);
+    reason = (row.size() == 8) ? row[6] : "";
+    set_order_id();
+    set_transaction_time();
 }
 
-ReportEntry::ReportEntry(std::string client_order_id, std::string instrument, int side, int quantity, double price, std::string reason) {
+ReportEntry::ReportEntry(std::string client_order_id, std::string instrument, int side, int quantity, double price, int status, std::string reason="") {
     
     this->client_order_id = client_order_id;
     this->instrument = instrument;
     this->side = side;
     this->quantity = quantity;
     this->price = price;
+    this->status = status;
     this->reason = reason;
+    set_order_id();
+    set_transaction_time();
+}
+
+void ReportEntry::set_order_id() {
+    
+    order_id = "ord" + std::to_string(MyGlobals::ORDER_ID);
+    MyGlobals::ORDER_ID++;
+}
+
+void ReportEntry::set_transaction_time() {
+    
+    auto currentTime = std::chrono::system_clock::now();
+    std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
+    std::tm* localTime = std::localtime(&currentTime_t);
+
+    std::ostringstream oss;
+    oss << std::put_time(localTime, "%Y-%m-%d %H:%M:%S");
+
+    transaction_time = oss.str();
 }
