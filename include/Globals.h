0f3a3271172ab050d3ed8b94e10510fd9c7c4198
Kalana Rubasinghe <kalanarajika99@gmail.com>
2023-12-15 12:49:12 +0530

    globals

---
+++ include/Globals.h
@@ -0,0 +1,26 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <string>
+#include <set>
+#include <unordered_map>
+
+namespace MyGlobals {
+
+    const std::set<std::string> INSTRUMENTS = {"Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
+    enum class SIDES : int {
+        BUY = 1,
+        SELL = 2
+    };
+    enum class STATUS : int {
+        NEW = 0,
+        REJECTED = 1,
+        FILL = 2,
+        PFILL = 3
+    };
+    // can add reasons here
+
+    extern int ORDER_ID;
+}
+
+#endif
